63085293a2f2655a6ec8d532f6f27a796b7a1a38
Norman Liu <normanwuweiliu@gmail.com>
2021-01-30 23:12:32 -0800

    added gzip option

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './client/index.jsx',
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/client'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new NodePolyfillPlugin(),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new LodashModuleReplacementPlugin(),
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    }),
+    new CompressionPlugin()
+    // new webpack.optimize.CommonChunkPlugin('react', 'react.bundle.js')
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+};
