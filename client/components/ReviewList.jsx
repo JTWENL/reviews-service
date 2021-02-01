ad811eb53dc4bc16f663c13c04d49d5cb4f8b678
Norman Liu <normanwuweiliu@gmail.com>
2021-02-01 15:19:33 -0800

    added sorting functions in modal

--- client/components/ReviewList.jsx
+++ client/components/ReviewList.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReviewHeader, {placeHolders} from './ReviewHeader.jsx';
 import StatsTable from './StatsTable.jsx';
-import { fmtStar, prettyDate } from '../renderHelpers.js';
+import Selector from './Selector.jsx';
+import Directional from './Directional.jsx';
+import { fmtStar, prettyDate, sortReviewsBy } from '../renderHelpers.js';
 
 const fmtRecommend = (b) => b ? (<p>✅ Yes, I recommend this product</p>) : null;
-
 
 const ReviewListItem = (props) => {
   let { review } = props;
@@ -27,17 +28,40 @@ class ReviewList extends React.Component {
   constructor(props) {
     super(props);
     let { reviews } = props;
+    this.onChange = this.onChange.bind(this);
+    this.onFlip = this.onFlip.bind(this);
     this.state = {
       reviews,
+      sortAsc: true
     };
+  }
+
+  onChange(ev) {
+    ev.preventDefault();
+    this.setState((st) => ({
+      reviews: sortReviewsBy(ev.target.value, st.reviews)
+    }));
+  }
+
+  onFlip(sortAsc) {
+    this.setState({sortAsc});
   }
 
   render() {
     let { reviews } = this.state;
-
     return (
       <div className='review-list'>
         <ReviewHeader reviews={reviews} />
+        <div className="review-sorter">
+          <Selector 
+            keys={['stars', ...Object.keys(reviews[0]?.metrics)]}
+            name='sort-options'
+            onChange={this.onChange}
+            label='Sort by: '
+          />
+          <Directional onClick={this.onFlip} />
+
+        </div>
         {reviews.map((r, i) => (
           <ReviewListItem key={i} review={r} />
         ))}
